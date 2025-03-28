ecab595a4e4760f3144046131766a38161a47236
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-03-28 02:25:15 +0200

    Home page Cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ThreeBgComponent } from "../three-bg/three-bg.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ThreeBgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
