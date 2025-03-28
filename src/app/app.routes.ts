a63a265d91be40aa3cda5ac70b1a40d5ac235b61
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-03-28 04:53:37 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { NavigationComponent } from './navigation/navigation.component';
-
-export const routes: Routes = [
-    {path:'',component:NavigationComponent},
-];
