f2f4178d42d66cc55fc0ced10b591358119a8588
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-03-28 03:56:50 +0200

    Started with skills Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { HomeComponent } from "./home/home.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, HomeComponent],
+  imports: [RouterOutlet, NavigationComponent, HomeComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
