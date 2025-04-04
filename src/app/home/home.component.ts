1d3f5fb63c502a9aee753cc4f99bccd3ba205891
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-04 22:01:07 +0200

    Added content for the about me section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ThreeBgComponent } from "../three-bg/three-bg.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ThreeBgComponent],
+  imports: [ThreeBgComponent, AboutMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
