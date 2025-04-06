78083ccee3ceba62a07d2beb3a288118e43b5387
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-06 22:49:37 +0200

    Contact Me Component Prep

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ThreeBgComponent } from "../three-bg/three-bg.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { ResumeComponent } from "../resume/resume.component";
+import { ContactMeComponent } from "../contact-me/contact-me.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ThreeBgComponent, AboutMeComponent, ResumeComponent],
+  imports: [ThreeBgComponent, AboutMeComponent, ResumeComponent, ContactMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
