6d72fe9d0636b218648a0ca59b8689708ef2dba0
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-08 01:18:11 +0200

    Responsiveness

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,28 @@ import { ThreeBgComponent } from "../three-bg/three-bg.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { ResumeComponent } from "../resume/resume.component";
 import { ContactMeComponent } from "../contact-me/contact-me.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ThreeBgComponent, AboutMeComponent, ResumeComponent, ContactMeComponent],
+  imports: [ThreeBgComponent, AboutMeComponent, ResumeComponent, ContactMeComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  showContacts = false;
+  isMobileScreen = false;
 
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  checkScreenSize() {
+    this.isMobileScreen = window.innerWidth <= 767;
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkScreenSize.bind(this));
+  }
 }
