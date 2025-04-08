8af191ef21fb248a01de10bd2192680395cbbaf0
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-08 03:23:57 +0200

    minor changes on resume colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { HomeComponent } from "./home/home.component";
-import { AboutMeComponent } from "./about-me/about-me.component";
-import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, HomeComponent, AboutMeComponent, SkillsComponent],
+  imports: [NavigationComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
