68bfd9f9ac144aaf6641508534ec67316fd82e56
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-17 21:02:46 +0200

    Alignment fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { ThreeBgComponent } from "../three-bg/three-bg.component";
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router'; 
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "../navigation/navigation.component"; 
 
 
 @Component({
   selector: 'app-home',
-  imports: [ThreeBgComponent,CommonModule, RouterOutlet], 
+  imports: [ThreeBgComponent, CommonModule, RouterOutlet, NavigationComponent], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   showContacts = false;
   isMobileScreen = false;
+  isMenuActive = false;
+
+  toggleMenu() {
+    this.isMenuActive = !this.isMenuActive;
+  }
 
   ngOnInit() {
     this.checkScreenSize();
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; // Import RouterLink and RouterOutlet
+import { RouterLink,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink],
+  imports: [RouterLink,RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
