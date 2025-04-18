85563b2ec9afd871c983bd00591c4dbcf20851f2
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-04-18 18:02:56 +0200

    Added a blue print for all the projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  imports: [CommonModule],
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    { name: 'School IMS', bgColor: '#7e7e7e', image: 'assets/' },
+    { name: 'Food App', bgColor: '#ff5757', image: 'assets/' },
+    { name: 'Florist App', bgColor: '#7dd87d', image: 'assets/' },
+    { name: 'Roomza Connect', bgColor: '#ffffff', image: 'assets/' },
+    { name: 'Quiz App', bgColor: '#ec1c8f', image: 'assets/' },
+  ];
 }
