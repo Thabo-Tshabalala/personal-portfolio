907dc86faad324895a9b8e69982cd4cd5f73c133
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-05-09 23:58:43 +0200

    projects prep

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjectsComponent {
   projects = [
-    { name: 'School IMS', bgColor: '#7e7e7e', image: 'assets/' },
+    { name: 'School IMS', bgColor: '#7e7e7e', image: 'assets/luffy.jpg' },
     { name: 'Food App', bgColor: '#ff5757', image: 'assets/' },
     { name: 'Florist App', bgColor: '#7dd87d', image: 'assets/' },
     { name: 'Roomza Connect', bgColor: '#ffffff', image: 'assets/' },
