2076cbc9afe7593db41a3d1286436dd7876f968c
Thabo-Tshabalala <justicethabo21@gmail.com>
2025-06-02 21:11:58 +0200

    rm roomzaconnect project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,7 +12,6 @@ export class ProjectsComponent {
     { name: 'School IMS', bgColor: '#7e7e7e', image: 'assets/school-ims.jpg',githubLink: 'https://github.com/Thabo-Tshabalala/School-IMS-FrontEnd' },
     { name: 'Meals App', bgColor: '#ff5757', image: 'assets/meals_app.jpg' ,githubLink: 'https://github.com/Thabo-Tshabalala/Meals_App'},
     { name: 'Florist App', bgColor: '#7dd87d', image: 'assets/florist.jpg',githubLink: 'https://github.com/Thabo-Tshabalala/Florist-Web-Application-Client-Side' },
-    { name: 'Roomza Connect', bgColor: '#ffffff', image: 'assets/roomza.jpg' ,githubLink: 'https://github.com/Thabo-Tshabalala/roomza-connect-front-end'},
     { name: 'Quiz App', bgColor: '#ec1c8f', image: 'assets/quiz app logo.jpg',githubLink: 'https://github.com/Thabo-Tshabalala/QuizApp' },
   ];
 }
