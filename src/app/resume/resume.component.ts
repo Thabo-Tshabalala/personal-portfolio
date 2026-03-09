fb41e56815af25ac918e77604942f717924e8380
Thabo-Tshabalala <justicethabo21@gmail.com>
2026-03-09 23:09:08 +0200

    update job role

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,32 +5,51 @@ import { CommonModule } from '@angular/common';
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
 export class ResumeComponent {
   education = [
-            {
+    {
+      institution: 'Cape Peninsula University of Technology',
+      degree:
+        'Postgraduate Diploma in Information Communication and Technology (Software Engineering)',
+      major: 'Software Development',
+      duration: '2026 - Present',
+    },
+    {
       institution: 'Cape Peninsula University of Technology',
       degree: 'Advanced Diploma in Information Communication and Technology',
       major: 'Software Development',
-      duration: '2025 — Present'
+      duration: '2025 — 2025',
     },
-    
+
     {
       institution: 'Cape Peninsula University of Technology',
       degree: 'Diploma in Information Communication and Technology',
       major: 'Software Development',
-      duration: '2022 — 2024'
+      duration: '2022 — 2024',
     },
 
-        {
+    {
       institution: 'Bongane Lebohang Secondary School',
       degree: 'National Senior Certificate (Matriculation)',
-      duration: '2018 — 2020'
+      duration: '2018 — 2020',
     },
   ];
 
   experience = [
+    {
+      title: 'Software Developer',
+      company: 'Pepkor NexTech',
+      duration: 'January, 2026 - Present',
+      location: 'Cape Town, Hybrid',
+      responsibilities: [
+        'Develop and maintain internal software solutions that support PEP business operations.',
+        'Implement new features and resolve bugs to improve internal systems.',
+        'Work closely with other developers to deliver reliable software solutions.',
+        'Participate in team discussions on project progress, fixes, and feature development.',
+      ],
+    },
     {
       title: 'Software Developer Intern',
       company: 'Onsight Software',
@@ -43,8 +62,7 @@ export class ResumeComponent {
         'Gained hands-on experience in debugging and troubleshooting application issues. ',
         'Collaborated with other developers to deliver features.',
         'Participated in team meetings to discuss new features and project updates.',
-      ]
+      ],
     },
   ];
-
 }
